44ace53a16c40e9093b979e0fc44d338165b7308
GrowingSky <63eric36@gmail.com>
2026-07-10 01:20:12 -0300

    Format main.py by adding newlines for improved readability

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import uvicorn
 from fastapi import FastAPI
 from lol_router import lol_router
 
 api = FastAPI()
 
 api.include_router(lol_router)
+
+
+if __name__ == "__main__":
+  uvicorn.run(api, host="0.0.0.0")
